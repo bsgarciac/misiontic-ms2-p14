73c67f7cf5b942f8ab21c43571efd40882d9dc17
Brayan Garcia <brayan.garcia@puntopay.co>
2021-11-22 10:48:20 -0500

    Dockerizacion

---
+++ src/main/java/com/misiontic/account_ms/controllers/AccountController.java
@@ -0,0 +1,41 @@
+package com.misiontic.account_ms.controllers;
+import com.misiontic.account_ms.exceptions.AccountNotFoundException;
+import org.springframework.web.bind.annotation.*;
+import com.misiontic.account_ms.models.Account;
+import com.misiontic.account_ms.repositories.AccountRepository;
+
+import java.util.Optional;
+
+@RestController
+public class AccountController {
+    private final AccountRepository accountRepository;
+
+    public AccountController(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @PostMapping("/accounts")
+    Account newAccount(@RequestBody Account account){
+        return accountRepository.save(account);
+    }
+
+    @GetMapping("/accounts/{username}")
+    Account getAccount(@PathVariable String username){
+        return accountRepository.findById(username)
+                .orElseThrow(() -> new AccountNotFoundException("No se encontró la cuenta"));
+    }
+
+    @DeleteMapping("/accounts/{username}")
+    void deleteAccount(@PathVariable String username){
+        accountRepository.deleteById(username);
+    }
+
+    @PutMapping("/accounts/{username}")
+    Account updateAccount(@PathVariable String username, @RequestBody Account new_account){
+        Account old_account = accountRepository.findById(username).orElse(null);
+        old_account.setLastChange(new_account.getLastChange());
+        old_account.setBalance(new_account.getBalance());
+        old_account.setUsername(new_account.getUsername());
+        return accountRepository.save(old_account);
+    }
+}
---
+++ src/main/java/com/misiontic/account_ms/controllers/TransactionController.java
@@ -0,0 +1,45 @@
+package com.misiontic.account_ms.controllers;
+
+import com.misiontic.account_ms.exceptions.AccountNotFoundException;
+import com.misiontic.account_ms.models.Account;
+import com.misiontic.account_ms.models.Transaction;
+import com.misiontic.account_ms.repositories.AccountRepository;
+import com.misiontic.account_ms.repositories.TransactionRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+@RestController
+public class TransactionController {
+    private final TransactionRepository transactionRepository;
+    private final AccountRepository accountRepository;
+
+    public TransactionController(TransactionRepository transactionRepository,AccountRepository accountRepository) {
+        this.transactionRepository = transactionRepository;
+        this.accountRepository = accountRepository;
+    }
+
+    @GetMapping("/transaction/{id}")
+    Optional<Transaction> getTransaction(@PathVariable  String id){
+        return transactionRepository.findById(id);
+    }
+
+    @GetMapping("/transactions")
+    List<Transaction> listTransactions(){
+        return transactionRepository.findAll();
+    }
+
+    @PostMapping("/transaction")
+    Transaction newTransaction(@RequestBody Transaction transaction){
+        Account accountDestiny = accountRepository.findById(transaction.getUsernameDestiny()).orElse(null);
+        Account accountOrigin = accountRepository.findById(transaction.getUsernameOrigin()).orElse(null);
+        if(accountDestiny == null || accountOrigin == null){
+            throw new AccountNotFoundException("No se encontró la cuenta destino o la cuenta destino");
+        }
+        accountOrigin.setBalance(accountOrigin.getBalance() - transaction.getValue());
+        accountDestiny.setBalance(accountDestiny.getBalance() + transaction.getValue());
+        accountRepository.save(accountDestiny);
+        accountRepository.save(accountOrigin);
+        return transactionRepository.save(transaction);
+    }
+}
